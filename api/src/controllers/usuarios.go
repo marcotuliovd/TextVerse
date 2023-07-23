84558d2d694b29e393dd21eb5b6568e6a01a725d
Marco Túlio <marcotuliodiniz2257@gmail.com>
2023-07-23 19:36:24 -0300

    🚚 create routes basic

---
+++ api/src/controllers/usuarios.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "net/http"
+
+func CreateUser(w http.ResponseWriter, r http.Response) {
+	w.Write([]byte("criando user"))
+}
+
+func FindUser(w http.ResponseWriter, r http.Response) {
+	w.Write([]byte("busca todos"))
+}
+
+func FindUserbyId(w http.ResponseWriter, r http.Response) {
+	w.Write([]byte("busca por id"))
+}
+
+func UpdateUser(w http.ResponseWriter, r http.Response) {
+	w.Write([]byte("atualiza user"))
+}
+
+func DeleteUser(w http.ResponseWriter, r http.Response) {
+	w.Write([]byte("criand user"))
+}
